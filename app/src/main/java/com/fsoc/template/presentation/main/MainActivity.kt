e6f3820842f72fadbc2e8e456d7b6c064125ac4e
Cuong <vocaocuong1998@gmail.com>
2021-08-19 20:13:33 +0700

    Dev screen xo so truc tiep

--- app/src/main/java/com/fsoc/template/presentation/main/MainActivity.kt
+++ app/src/main/java/com/fsoc/template/presentation/main/MainActivity.kt
@@ -66,7 +66,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                 R.id.nav_listMessage,
                 R.id.nav_listCustomerFragment,
                 R.id.nav_setting,
-                R.id.nav_message_template
+                R.id.nav_message_template,
+                R.id.nav_live_lottery
             ), binding.drawerLayout
         )
         val mNavController = findNavController(R.id.nav_host_fragment_content)
